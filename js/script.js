474bbc745679ac7e14db4fd9fb3b1e6de6e2c760
FERREIRAJeferson <Jefersonferreira8807@gmail.com>
2026-03-25 21:42:51 -0300

    Alinhado a main

--- js/script.js
+++ js/script.js
@@ -55,21 +55,21 @@ if (themeToggleBtn) {
     document.body.classList.add("dark-mode");
     if (themeIcon) themeIcon.classList.replace("fa-moon", "fa-sun");
   }
+
+  themeToggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    const isDark = document.body.classList.contains("dark-mode");
+
+    if (isDark) {
+      if (themeIcon) themeIcon.classList.replace("fa-moon", "fa-sun");
+      localStorage.setItem("theme", "dark");
+    } else {
+      if (themeIcon) themeIcon.classList.replace("fa-sun", "fa-moon");
+      localStorage.setItem("theme", "light");
+    }
+  });
 }
-
-themeToggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
-  const isDark = document.body.classList.contains("dark-mode");
-
-  if (isDark) {
-    if (themeIcon) themeIcon.classList.replace("fa-moon", "fa-sun");
-    localStorage.setItem("theme", "dark");
-  } else {
-    if (themeIcon) themeIcon.classList.replace("fa-sun", "fa-moon");
-    localStorage.setItem("theme", "light");
-  }
-});
 
 const slidesContainer = document.querySelector(".slides");
 const imagens = document.querySelectorAll(".slides img");
